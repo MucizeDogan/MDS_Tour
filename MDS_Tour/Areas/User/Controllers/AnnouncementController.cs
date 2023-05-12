61c34391cbfe9a97538936d6354bd081e7e3a3fa
M-racle <108946729+M-racle@users.noreply.github.com>
2023-05-12 10:46:25 +0300

    Improvements were made on the template and dashboard. Username and image values have been moved via Layout.

--- MDS_Tour/Areas/User/Controllers/AnnouncementController.cs
+++ MDS_Tour/Areas/User/Controllers/AnnouncementController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using BusinessLayer.Abstract;
 using DTOLayer.DataTrabsferObjects.AnnouncementDTOs;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MDS_Tour.Areas.User.Controllers
@@ -10,21 +12,29 @@ namespace MDS_Tour.Areas.User.Controllers
     [AllowAnonymous]
     public class AnnouncementController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
         private readonly IAnnouncementService _announcementService;
         private readonly IMapper _mapper;
 
-        public AnnouncementController(IAnnouncementService announcementService, IMapper mapper)
+        public AnnouncementController(IAnnouncementService announcementService, IMapper mapper, UserManager<AppUser> userManager)
         {
             _announcementService = announcementService;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
+                var data = _mapper.Map<List<AnnouncementListDTO>>(_announcementService.TGetList());
+                var values = await _userManager.FindByNameAsync(User.Identity.Name);
 
-            var data = _mapper.Map<List<AnnouncementListDTO>>(_announcementService.TGetList());
-            return View(data);
+                if (values != null)
+                {
+                    ViewBag.userName = values.Name +" "+ values.Surname;
+                    ViewBag.userImage = values.Image;
+                }
 
+                return View(data);
         }
     }
 }
